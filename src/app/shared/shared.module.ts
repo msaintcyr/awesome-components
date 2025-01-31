68e6864c180222863c1d3bf49f0c345094de1e50
Micke <mickael.saintcyr@gmail.com>
2025-01-31 15:56:33 +0100

    20250131.0

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,9 +7,7 @@ import { MaterialModule } from './material.module';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { TimeAgoPipe } from './pipes/time-ago';
 import { UsernamePipe } from './pipes/username.pipe';
-import { TimeAgoPipe } from './pipes/time-ago';
-import { HighlightDirective } from './directives/highlight.directive';
-import { RouterLink } from '@angular/router';
+import { MatError } from '@angular/material/form-field';
 
 @NgModule({
   imports: [
@@ -20,7 +18,10 @@ import { RouterLink } from '@angular/router';
     ShortenPipe,
     UsernamePipe,
     TimeAgoPipe,
-    HighlightDirective
+    HighlightDirective,
+    NgIf,
+    NgFor,
+    AsyncPipe
   ],
   exports: [
     RouterLink,
@@ -29,7 +30,10 @@ import { RouterLink } from '@angular/router';
     ShortenPipe,
     UsernamePipe,
     TimeAgoPipe,
-    HighlightDirective
+    HighlightDirective,
+    NgIf,
+    NgFor,
+    AsyncPipe
   ]
 })
 export class SharedModule { }
